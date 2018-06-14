98fc093b2b256848db884acf6b76ff4a72df35ba
Richard Cheney <richard.cheney@microsoft.com>
2018-06-14 11:24:17 +0100

    Added in two more NSGs

--- nsgs.tf
+++ nsgs.tf
@@ -69,3 +69,45 @@ resource "azurerm_network_security_rule" "AllowHTTPS" {
     source_port_range           = "*"
     source_address_prefix       = "*"
 }
+resource "azurerm_network_security_group" "AllowSQLServer" {
+   name = "AllowSQLServer"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowSQLServer" {
+    name = "AllowSQLServer"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowSQLServer.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 1443
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+resource "azurerm_network_security_group" "AllowRDP" {
+   name = "AllowRDP"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowRDP.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
